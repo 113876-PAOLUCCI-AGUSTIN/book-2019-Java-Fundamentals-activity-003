eaf5af5205ccf1b1ebde9260e810e72d93cea399
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-03 20:17:22 -0300

    STDIN created.

--- src/StockChangeCalculator.java
+++ src/StockChangeCalculator.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 
 public class StockChangeCalculator {
     public static void main(String[] args) {
-
+        // STDIN
+        System.out.println("Enter the stock symbol: ");
+        Scanner input = new Scanner(System.in);
+        String ticker = input.nextLine();
+        System.out.println("Enter the "+ticker+"'s day 1 value: ");
+        Double day1price = Double.valueOf(input.next());
+        System.out.println("Enter the "+ticker+"'s day 2 value: ");
+        Double day2price = Double.valueOf(input.next());
     }
 }
